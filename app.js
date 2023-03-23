354a61ae72610a935d6636d1bfe883b0d5d874ff
Dom <122333281+DomenicGTH@users.noreply.github.com>
2023-03-23 05:50:03 +0000

    easier to read, button clears after input

--- app.js
+++ app.js
@@ -17,18 +17,20 @@ function addTodo(e) {
   const div = document.createElement("div"); // creates div
   div.classList.add("todo"); // adds todo class to created div
   const makeli = document.createElement("li");
-  makeli.innerText = "Test";
+  makeli.innerText = todoinput.value; // pulls the text from the <input>
   makeli.classList.add("newtodo");
   div.appendChild(makeli); // appends makeli to div
-  const buttonFinish = document.createElement("button");
-  buttonFinish.innerText = "test";
-  buttonFinish.classList.add("buttonDone");
-  div.appendChild(buttonFinish);
+  const buttonFinish = document.createElement("button"); // creates button
+  buttonFinish.innerText = "test"; // adds text to button
+  buttonFinish.classList.add("buttonDone"); // adds a class of buttonDone to the buttom
+  div.appendChild(buttonFinish); // appends button to div
 
   const buttonDelete = document.createElement("button");
   buttonDelete.innerText = "testDelete";
   buttonDelete.classList.add("buttonDelete");
-  div.appendChild(buttonDelete);
-
+  div.appendChild(buttonDelete); // ditto buttonFinish
   todoList.appendChild(div);
+  todoinput.value = ""; // clear todoinput value
 }
+
+// TIME: 30:00
