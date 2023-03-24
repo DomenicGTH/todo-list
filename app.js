dba70d9ad40e982a630b17eb8ad74cb3b9764191
Dom <122333281+DomenicGTH@users.noreply.github.com>
2023-03-24 05:59:41 +0000

    added delete functionality to local storage

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const addTodo = (e) => {
   localSave(todoinput.value); // runs the localSave function, stores the todoinput.value
   const buttonFinish = document.createElement("button"); // creates button
   buttonFinish.innerHTML = '<i class="fa-sharp fa-solid fa-circle-check"></i>'; // adds icon to button
-  buttonFinish.classList.add("buttonDone"); // adds a class of buttonDone to the buttom
+  buttonFinish.classList.add("buttonDone"); // adds a class of buttonDone to the button
   div.appendChild(buttonFinish); // appends button to div
 
   const buttonDelete = document.createElement("button");
@@ -43,6 +43,7 @@ const deleteTodo = (e) => {
     // classList[0] = checks first class
     const todoParent = item.parentElement; // todoParent becomes the parent element of what we clicked on
     todoParent.classList.add("delete-anim");
+    removeLocal(todoParent);
     todoParent.addEventListener("transitionend", (e) => {
       // will excute after transition finishes
       todoParent.remove();
@@ -100,6 +101,51 @@ const localSave = (e) => {
   // JSON string is a data format
 };
 
+const getLocalStorage = (e) => {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = []; // same as localSave, if there is nothing in local storage, create empty array
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos")); // if there is something in local storage, obtain it
+  }
+  todos.forEach((e) => {
+    // creating the list again, takes every stored item in the array and adds the appropriate properties to it
+    const div = document.createElement("div");
+    div.classList.add("todo");
+    const makeli = document.createElement("li");
+    makeli.innerText = e;
+    makeli.classList.add("newtodo");
+    div.appendChild(makeli);
+    const buttonFinish = document.createElement("button");
+    buttonFinish.innerHTML =
+      '<i class="fa-sharp fa-solid fa-circle-check"></i>';
+    buttonFinish.classList.add("buttonDone");
+    div.appendChild(buttonFinish);
+
+    const buttonDelete = document.createElement("button");
+    buttonDelete.innerHTML = '<i class="fa-solid fa-circle-minus"></i>'; // same as above
+    buttonDelete.classList.add("delete-btn");
+    div.appendChild(buttonDelete);
+    todoList.appendChild(div);
+  });
+};
+
+const removeLocal = (e) => {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = e.children[0].innerText; // navigates to the current index, then displays the text of the current index
+  console.log(e.children[0].innerText);
+  todos.splice(todos.indexOf(todoIndex), 1); // this gets the current index of chosen todoIndex, then removes it
+  // the 1 being how many items it needs to remove
+  localStorage.setItem("todos", JSON.stringify(todos));
+  // call this again to update the local storage
+};
+
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteTodo);
 dropDown.addEventListener("mouseover", todoDropdown);
+addEventListener("DOMContentLoaded", getLocalStorage);
